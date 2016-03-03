ce482d81a0d5b90d972669cc6443ae9485df03fc
Maxim <maxshaptala@gmail.com>
2016-03-03 21:06:05 +0200

    добавлен фильтр

--- Lesson13/Lesson13/src/ua/itstep/shaptala/examples/io/WordCounter.java
+++ Lesson13/Lesson13/src/ua/itstep/shaptala/examples/io/WordCounter.java
@@ -14,66 +14,69 @@ import java.util.TreeMap;
 
 public class WordCounter {
 
-	static class Word implements Comparable<Word>{
+	static class Word implements Comparable<Word> {
 		String content;
 		Integer count;
-		
+
 		Word(String content, Integer count) {
 			this.content = content;
 			this.count = count;
 		}
+
 		@Override
-		public int compareTo(Word o) {			
+		public int compareTo(Word o) {
 			return o.count - count;
 		}
-		
+
 		@Override
-		public String toString() {		
-			return content+": " + count;
+		public String toString() {
+			return content + ": " + count;
 		}
-				
+
 	}
-	
+
 	String fname;
 	Map<String, Integer> words = new TreeMap<>();
+
 	public WordCounter(String fname) {
 		this.fname = fname;
 	}
-	
+
 	List<Word> getMostUsing(int count) {
 		List<Word> mostUsing = new ArrayList<>();
-		try(Scanner scanner = new Scanner(new InputStreamReader(new BufferedInputStream(new FileInputStream(fname)), "UTF8"))){
-			while(scanner.hasNext()){
-				final String regex = "[_\\.,:;\"\'\\)\\(\\?\\!\\-]";				
+		try (Scanner scanner = new Scanner(
+				new InputStreamReader(new BufferedInputStream(new FileInputStream(fname)), "UTF8"))) {
+			while (scanner.hasNext()) {
+				final String regex_filter = "[_\\.,:;\"\'\\)\\(\\?\\!\\-\\>\\<]";
 				final String regex_rus_words = "[Р-пр-џ]{4,}";
-				
-				String word = scanner.next().replaceAll(regex, "").toLowerCase();
-				if(word.matches(regex_rus_words)) {					
-					if(!words.containsKey(word)) {
+
+				String word = scanner.next().replaceAll(regex_filter, "").toLowerCase();
+				if (word.matches(regex_rus_words)) {
+					if (!words.containsKey(word)) {
 						words.put(word, 1);
 					} else {
-						words.put(word, words.get(word)+1);
+						words.put(word, words.get(word) + 1);
 					}
 				}
 			}
-			
-			for(String w: words.keySet()) {
+
+			for (String w : words.keySet()) {
 				Word nw = new Word(w, words.get(w));
-				mostUsing.add(nw);				
-			}			
+				mostUsing.add(nw);
+			}
 			mostUsing.sort(null);
-			
-		} catch (UnsupportedEncodingException e) {			
+
+		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
-		} catch (FileNotFoundException e) {			
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		return mostUsing.subList(0, count);
 	}
-	
+
 	public static void main(String[] args) {
 		WordCounter wordCounter = new WordCounter("alice.txt");
-		for(Word word: wordCounter.getMostUsing(5)) {
+		for (Word word : wordCounter.getMostUsing(5)) {
 			System.out.println(word);
 		}
 	}
